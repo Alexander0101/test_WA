485444f5063fe06810d675559bd2950123b72647
Alexander0101 <ovcharenko0101sasha@gmail.com>
2019-06-08 00:33:08 +0300

    add some changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+	$('.nav-tabs li').click(function() {
+		if (!$(this).hasClass('active')) {
+			var i = $(this).index();
+			$('.nav-tabs li.active').removeClass('active');
+			$('.tabs-content .active').hide().removeClass('active');
+			$(this).addClass('active');
+			$($('.tabs-content').children('.content-item')[i]).fadeIn(1000).addClass('active');
+		}
+	});
 	$('.banner-slider').slick({
 	  dots: true,
 	  arrows: false,
