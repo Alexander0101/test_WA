8133f0537535871116877f3aff4b54cc4d8515b8
Alexander0101 <ovcharenko0101sasha@gmail.com>
2019-06-11 22:36:57 +0300

    add scrollbar and some changes to slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
 $(document).ready(function() {
-	$('.nav-tabs li').click(function() {
+  var mainSlider = $('.banner-slider');
+  var tabSlider1 = $('.slider-container-tab1');
+  var tabSlider2 = $('.slider-container-tab2');
+  var slickOptions = {
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1720,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  $('.scrollbar-rail').scrollbar();
+	$('.nav-tab').click(function() {
 		if (!$(this).hasClass('active')) {
 			var i = $(this).index();
-			$('.nav-tabs li.active').removeClass('active');
+			var activeSlider = '';
+			if (i === 0) activeSlider = tabSlider1;
+      if (i === 1) activeSlider = tabSlider2;
+      if (tabSlider1.hasClass('slick-initialized')) tabSlider1.slick('unslick');
+      if (tabSlider2.hasClass('slick-initialized')) tabSlider2.slick('unslick');
+      activeSlider.slick(slickOptions);
+			$('.nav-tab.active').removeClass('active');
 			$('.tabs-content .active').hide().removeClass('active');
 			$(this).addClass('active');
 			$($('.tabs-content').children('.content-item')[i]).fadeIn(1000).addClass('active');
 		}
 	});
-	$('.banner-slider').slick({
+	mainSlider.slick({
 	  dots: true,
 	  arrows: false,
 	  infinite: true,
@@ -17,31 +54,5 @@ $(document).ready(function() {
 	  autoplay: true,
   	autoplaySpeed: 5000
 	});
-	$('.slider-container').slick({
-	  dots: false,
-	  autoplay: true,
-  	autoplaySpeed: 3000,
-	  infinite: true,
-	  speed: 700,
-	  slidesToShow: 3,
-	  slidesToScroll: 3,
-	  responsive: [
-	    {
-	      breakpoint: 1720,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2,
-	        infinite: true,
-	        dots: false
-	      }
-	    },
-	    {
-	      breakpoint: 1200,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }
-	  ]
-	});
+  tabSlider1.slick(slickOptions);
 });
